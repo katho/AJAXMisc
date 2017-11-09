4a97279352450a8a7b555d80a7611d026cef0d39
katho <katarino240sx@gmail.com>
2017-11-09 08:53:17 -0600

    Se agrega plantilla.html y secondary_controller.js

---
+++ AJAXMisc/WebContent/js/secondary_controller.js
@@ -0,0 +1,6 @@
+$(document).ready( function(){
+    //Esta plantilla le indica a JavaScript que realice un trabajo una vez cargada la página
+    //A partir de este punto se pueden crear funciones para realizar control lógico sobre el HTML
+     //Clear console
+     console.clear();
+});
